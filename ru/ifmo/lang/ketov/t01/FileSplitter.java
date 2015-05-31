69e8bbaaab378805175e199eba5283265abdbecc
Artem <artem_molot@list.ru>
2015-05-31 22:53:12 +0300

    Исправлено

---
+++ ru/ifmo/lang/ketov/t01/FileSplitter.java
@@ -0,0 +1,35 @@
+package ru.ifmo.lang.ketov.t01;
+
+/**
+ * Created by Артём on 25.02.2015.
+ */
+public interface FileSplitter {
+    /**
+     * Разбивает файл на два отдельных
+     * @param config настройки расположения файлов
+     */
+    void splitFile(SplitConfig config);
+
+    /**
+     * Настройки для разбивания файла на части
+     */
+    interface SplitConfig {
+
+        /**
+         * @return путь до файла с входными данными
+         */
+        String getSourceFilePath();
+
+        /**
+         * @return путь до результирующего файла с нечетными строками
+         * (файл будет создан в процессе работы)
+         */
+        String getOddLinesFilePath();
+
+        /**
+         * @return путь до результирующего файла с четными строками
+         * (файл будет создан в процессе работы)
+         */
+        String getEvenLinesFilePath();
+    }
+}
